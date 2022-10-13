f393cfd30bef3b81ecd3f83a79c7a9a5b396c235
evgenii <evgenii.lyabov@mail.ru>
2022-10-13 21:11:33 +0300

    add sem9kr2

---
+++ sem9kr2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+System.Console.WriteLine("натуральные числа в промежутке от M до N с помощью рекурсии\n");
+System.Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число M ");
+int M = Convert.ToInt32(Console.ReadLine());
+between_M_and_N();
+void between_M_and_N(int from, int to)
+{
+    if (from == to) Console.Write(from);
+    
+    else
+    {
+        Console.Write(from + " ");
+        between_M_and_N(from + 1, to);
+    }
+}
+
