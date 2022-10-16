fe9439b86dc726e952ca2a522653e508351d55c0
evgenii <evgenii.lyabov@mail.ru>
2022-10-16 22:16:45 +0300

    add sem9dz66

---
+++ sem9dz66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+
+Console.Clear();
+System.Console.WriteLine("сумма натуральных элементов в промежутке от M до N с помощью рекурсии\n");
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+sumNum(M, N);
+int sumNum(int M, int N)
+{
+    int sum =0;
+    for (int i = N; i <= M; i++) sum+=i;
+    System.Console.WriteLine(sum);
+    return sum;
+}
