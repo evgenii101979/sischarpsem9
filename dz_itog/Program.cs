d3def8358b0cf866812c8fc8585164b78053c72f
evgenii <evgenii.lyabov@mail.ru>
2022-11-11 18:08:35 +0300

    add dz

--- dz_itog/Program.cs
+++ dz_itog/Program.cs
@@ -1,16 +1,45 @@
-﻿string[] M;
-            Console.WriteLine("Введите массив строк через пробел");
-            string stroka = Console.ReadLine();
-            M = stroka.Split(' ');
-            var result = new string[M .Length];
-            var realSize = 0;
-            foreach (var value in M )
-            {
-                if (value.Length <= 3)
-                {
-                    result[realSize] = value;
-                    realSize++;
-                }
-            }
-            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
-            Console.ReadKey(true);
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine());
+string[] startArray = new string[n];
+System.Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < n; i++)
+{
+    startArray[i] = Console.ReadLine();
+}
+Console.Write("Введенный массив: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(startArray[i] + ", ");
+}
+
+
+string[] endArray = new string[n];
+
+void FormArrayOfStrings(string[] startArray, string[] endArray)
+{
+    int count = 0;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (startArray[i].Length <= 3)
+        {
+            endArray[count] = startArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+    Console.WriteLine();
+}
+FormArrayOfStrings(startArray, endArray);
+Console.Write("Массив с 3 и менее символами: ");
+PrintArray(endArray);
